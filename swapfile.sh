9d5acd3df09cfff23b68b3adedb26d368a28e03c
skandyn-sh <39844333+skandyn-sh@users.noreply.github.com>
2018-11-19 18:36:06 +0100

    Create swapfile.sh

---
+++ swapfile.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "This script creates a swap file 1GB (path: /swapfile) and adds it to /etc/fstab"
+echo "Please enter your sudo password "
+sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile   
+sudo sh -c 'echo "/swapfile swap swap defaults 0 0" >> /etc/fstab'
+echo
+echo "We make sure it works"
+echo
+free -m
+echo
+echo "Your swap file is ok"   
+exit 
