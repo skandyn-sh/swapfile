b1ac07b238aa8bf6c6f60e5268908e483bb11e92
skandyn-sh <39844333+skandyn-sh@users.noreply.github.com>
2018-11-20 17:45:40 +0100

    Update swapfile.sh

--- swapfile.sh
+++ swapfile.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-echo "This script creates and activates a swap file 1GB (path: /swapfile) and then adds it to /etc/fstab"
-echo "Please enter your sudo password"
-sudo dd if=/dev/zero of=/swapfile bs=1M count=1024
+echo "This script creates and activates a swap file (path: /swapfile) and then adds it to /etc/fstab"
+echo "Please enter the swap file size, e.g. 1,2,4,8,16"
+read d
+case "$d" in
+  "1") sudo dd if=/dev/zero of=/swapfile bs=1M count=1024 ;;
+  "2") sudo dd if=/dev/zero of=/swapfile bs=1M count=2048 ;;
+  "4") sudo dd if=/dev/zero of=/swapfile bs=1M count=4096 ;;
+  "8") sudo dd if=/dev/zero of=/swapfile bs=1M count=8192 ;;
+  "16") sudo dd if=/dev/zero of=/swapfile bs=1M count=16384 ;;
+esac
+  
 sudo chmod 600 /swapfile
 sudo mkswap /swapfile
 sudo swapon /swapfile   
@@ -10,6 +18,4 @@ echo
 echo "We make sure it works"
 echo
 free -m
-echo
-echo "Your swap file is ok"   
 exit 
